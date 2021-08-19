3f998a19e043b629fa4822da37a9ed1c74eb7860
Yokesh <eryokeshuma@gmail.com>
2021-08-19 23:22:29 +0530

    moved nav from html to js

---
+++ nav.js
@@ -0,0 +1,11 @@
+var parent = document.body;
+var element = document.createElement('nav');
+parent.insertBefore(element,document.body.firstChild);
+document.querySelector('nav').innerHTML = `<a href="index.html"><img
+        src="https://assets.website-files.com/5e6c01bb5212506d6c119069/5ee401ee1f08215e6dbf0661_color-lettermark.svg"></a>
+<a href="services.html">SERVICES</a>
+<a href="security.html">SECURITY</a>
+<a href="team.html">TEAM</a>
+<a href="careers.html">CAREERS</a>
+<a href="blog.html">BLOG</a>
+<a href="contact.html">CONTACT</a>`
