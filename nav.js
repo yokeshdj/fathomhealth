b300fecab43e6ab0f08b12e557cc3b137b0cc772
pradip mandal <join2me.pm@gmail.com>
2021-08-22 15:25:31 +0530

    1)added mediaQuery on contact page 2)added mediaQuery on navbar 3)added mediaQuery on blog page

--- nav.js
+++ nav.js
@@ -10,7 +10,8 @@ document.querySelector('nav').innerHTML = `<a href="index.html"><img
 <a href="career.html">CAREERS</a>
 <a href="blog.html">BLOG</a>
 <a href="contact.html">CONTACT</a>
-</div>`;
+</div>`
+
 var navigationBar = document.querySelector('nav')
 var url = window.location.href;
 var page = ""
@@ -40,3 +41,29 @@ else{
         var ele = navigationBar.getElementsByTagName('a')[6];
         ele.setAttribute('class','text-color')
 }
+var parent = document.body;
+var element = document.createElement('div');
+parent.insertBefore(element,parent.firstChild);
+document.querySelector('div').innerHTML = 
+`<div class="topnav" id="myTopnav">
+  <a href="index.html" class="active"><img src="https://assets.website-files.com/5e6c01bb5212506d6c119069/5ee401ee1f08215e6dbf0661_color-lettermark.svg"></img></a>
+  <a href="services.html">SERVICES</a>
+  <a href="security.html">SECURITY</a>
+  <a href="team.html">TEAM</a>
+  <a href="career.html">CAREERS</a>
+<a href="blog.html">BLOG</a>
+<a href="contact.html">CONTACT</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>`;
+//for responsiv nav bar
+function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+//end
