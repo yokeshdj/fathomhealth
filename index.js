374fb22c8a9cf118c2793ac4afae05120a26f0e6
Yokesh <eryokeshuma@gmail.com>
2021-08-25 09:48:16 +0530

    index media query added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var element = document.querySelector('.faded-content-container');
 console.log(window.scrollY,element);
+console.log(element);
 window.addEventListener('onscrollY',function(){
 if(window.scrollY > 500){
     element.setAttribute('style','opacity:1')
